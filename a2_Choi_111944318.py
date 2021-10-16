a743a076eb75203d1d9438691a0b480b4128458c
hitboxes <74222497+hitboxes@users.noreply.github.com>
2021-10-16 14:22:25 -0400

    Update a2_Choi_111944318.py

--- a2_Choi_111944318.py
+++ a2_Choi_111944318.py
@@ -10,7 +10,25 @@ def schoolBinonmialHypTest(null_school, test_school, alpha, school_data):
     # test_state: the school representing the observed count
     # alpha: significance level
     # school_data: the school name, case counts, and population
-
+    null_school_name = school_data[0]
+    null_case_counts = school_data[1]
+    null_population = school_data[2]
+    null_probability = null_case_counts/null_population
+    test_school_name = school_data[3]
+    test_case_counts = school_data[4]
+    test_population = school_data[5]
+    test_probability = test_case_counts/test_population
+    null_dist = ss.binom(null_population, null_probability)
+    test_sample = test_probability * null_population
+    print(null_probability)
+    print(test_probability)
+    print(test_sample)
+    p = 1 - null_dist.cdf(test_sample)
+    decision = test_school_name
+    if(p < alpha):
+        decision = decision + ", Decision: reject; p: " + str(p)
+    else:
+        decision = decision + ", Decision: Failed to reject; p: " + str(p)
     return p, decision
 
 def schoolNormalHypTest(null_school, test_school, alpha, school_data):
@@ -18,17 +36,22 @@ def schoolNormalHypTest(null_school, test_school, alpha, school_data):
     # test_state: the school representing the observed count
     # alpha: significance level
     # school_data: the school name, case counts, and population
-
+    school_name = school_data[0]
+    case_counts = school_data[1]
+    population = school_data[2]
     return p, decision
 
     
 if __name__ == "__main__":
     # College Covid Case Counts
-    schoolBinonmialHypTest("Stony Brook University", "New York University", .05, "ny_colleges.csv")
+    sampleSchoolData = ["Stony Brook University", 1126, 29607.0, "New York University", 2389, 51123.0]
+    p, decision = schoolBinonmialHypTest("Stony Brook University", "New York University", .05, sampleSchoolData)
+    print(p)
+    print(decision)
     # Comparing SBU to Many New York Schools
 
     # Modeling with the normal
 
     # Standard Error based CI
-    
+
     # Bootstrapped CI
